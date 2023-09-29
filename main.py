bdca985ec0dcc269d5bd908fea65491de59dd706
LuizHenz <robertgito@hotmail.com>
2023-09-29 10:36:52 -0300

    OS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import platform
+import os
 
 def identificar_so():
     sistema = platform.system()
@@ -12,5 +13,27 @@ def identificar_so():
         print(distro['NAME'], distro['VERSION'])
     else:
         print(f'Sistema Operacional não reconhecido: {sistema}')
+    
+    return sistema
 
-identificar_so()
+
+def criar_diretorio(caminho, nome):
+    end_comp = os.path.join(caminho, nome)
+
+    try:
+        os.makedirs(end_comp)
+        print(f'Diretório criado: {end_comp}')
+    except OSError as error:
+        print(f'Erro: {error}')
+
+
+so = identificar_so()
+
+if 'Linux' == so:
+    endereco = '/home/k4rma/Documentos'
+    nome_diretorio = 'teste'
+    criar_diretorio(endereco,nome_diretorio)
+# else:
+#     endereco_win = 'C:\Users\<usuario>\Documents'
+#     nome_diretorio_win = 'teste'
+#     criar_diretorio(endereco_win,nome_diretorio_win)
